5fdf811b3700f22df3689a033bf4a4fe110a9bcf
YScare <161298525+YScare@users.noreply.github.com>
2024-03-05 13:01:50 +0800

    Add files via upload

---
+++ yudi.js
@@ -0,0 +1,12 @@
+/*:
+@author 千影衣
+@url https://github.com/YScare/RPG-MAKER-MZ-plugin
+@target MZ
+@plugindesc 隐藏战斗遇敌
+@help 商用OK，免费，无需署名
+*/
+(function(){
+    BattleManager.displayStartMessages = function() {
+        
+    };
+})();
